2c5a4996980672d75ad56aa9b87757e70c5474e1
agustin <agustin.piqueres@gmail.com>
2022-12-06 19:15:15 +0100

    doc: new example ready

--- examples/options_example_like.py
+++ examples/options_example_like.py
@@ -2,15 +2,45 @@
 https://github.com/jazzband/docopt-ng/blob/master/examples/options_example.py
 
 $ python examples/options_example_like.py
+I li. Nnnfeenhca. Talmonsao rsi. H arc ylwslreneol.
 
-usage: ybde hd [options] [--] <lh>
+Usage: 
+  lbdef ro --iiispn-atinn=OOCSID --lgt=NRO [-C] [-a] (-m=M-P | -e)
+  lbdef edr [--rmkni S] [--ign-tmua-ccop] [-a] GDABN-ATS...
+  lbdef oneib --whua=EPKSSL [-e] --crbheus [-s] -m [-t] [--y-rhiart=INYA]
+                   -t=PRLHL
 
-    -e, --eleen  Llhnirsue desepte rhpoddjw. Aa ep ssice tnnch uxignn clpe
-                 tts eucafe enct
-    -o, --oefl   Rnsrh sope noei eoan rerimd naok teoaie nrano. Ka r. Ern
-                 etint p
-    -n, --nnsge
-    -o, --os     Yyocvi aae oaedeae aahdu roueeriiy dstltno og hr
+Arguments:
+  GDABN-ATS  Tuilnbm r liob peae. Gisox houheiee lhdp roc.
+
+Options:
+  --iiispn-atinn=OOCSID  Ossecn eao lsson kte.
+  --lgt=NRO              Pitn affde tme or aieba pnca. Shupi oewslaer wcarrtcbm ohzobr
+                         s.
+  -C                     Oa awg aowdc fet e.
+  -a                     Fndvrc eejo oie. Oniank dhmtccfm hger teonie
+                         ochmpasnnempl akyebsn. Onnr rlwnrllngb eegty. Nrn
+                         ijvrrlg.
+  -m=M-P                 Vaiioop owp aca r cnrp aataemeegp. P elci golu ytom ntn.
+  -e                     Vdiemdue elerr eoiapf t ri ratet isagfd nawae eflialk
+                         nocu aioa. Evivri ic tio. W.
+  --rmkni S              Tae sdoyott sr roehi vwpo. Oam dcldii. Ge uhin isre ew.
+  --ign-tmua-ccop        Ea nee ars go elesi aoislg. Lanf r.
+  -a                     Ao ietc rh osi. Uo tr oaopbpi hrs bc ufyeerdo. Yyst
+                         nhaksriat rbeveu rmtenpq smu wr.
+  --whua=EPKSSL          Eoyksyq eyoo rlt oaain nhdn. Vd. Mir orhea csiseal moi.
+  -e                     Yaa ngad aioeer. Artieop tc. Laer habc. All sde ls tt
+                         er rbe. Gnerp elet lmunh cf gua lb pen tx ngrlepe.
+                         Cnu emopck daubt tc sn.
+  --crbheus              Baecy aueerhbul conx rce oren. Tph toeble oo cpuort nyeno na
+                         paainob.
+  -s                     Olbhccna ery. Aa rst koosnsrae fak le.
+  -m                     Eiud egtncts gleeee eae lao cl odoe.
+  -t                     Rla hso l olhir. Lplesn rl.
+  --y-rhiart=INYA        Ip ato utoeuh lemre ir csaouet tcanhor. T ons ktzmatcr. Eeml thcroud
+                         lrooa rarri iopob acuu.
+  -t=PRLHL               Gspel urrrrt icf. M hnbnsot pubs pas. S st mmau. Kyhaupt
+                         ooci. Leradop iaae.
 
 """
 
@@ -20,7 +50,24 @@ from cli_help_maker import generator as gen
 
 
 if __name__ == "__main__":
-    generator = gen.HelpGenerator()
+    generator = gen.HelpGenerator(
+        indent_spaces=2,
+        program_description_prob=1,
+        argument_style="all_caps",
+        argument_repeated=True,  # TODO: Needs a probability
+        arguments_header=True,
+        arguments_section=True,
+        argument_documented_prob=1.,
+        options_section=True,
+        options_header=True,
+        option_documented_prob=1,
+        options_style={"style": "all_caps"},
+        options_mutually_exclusive={"probability": 1/15, "group": 2},
+        number_of_commands=1,
+        number_of_options=[1, 8],
+        number_of_arguments=[0, 1],
+        exclusive_programs=3,
+    )
     msg = generator.sample()
     # PARA CONSEGUIR TODOS LOS ARGUMENTOS EN LA MISMA LINEA (same_line) UTILIZAR
     # TEXTWRAP INDENT, CON INDENT INICIAL DIFERENTE, DE FORMA QUE QUEDE ALINEADO
