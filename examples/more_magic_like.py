03eadd13fdf61ac41b96185e5ebb86183f8c569e
agustin <agustin.piqueres@gmail.com>
2022-12-14 22:00:09 +0100

    doc: added arguments section

--- examples/more_magic_like.py
+++ examples/more_magic_like.py
@@ -1,5 +1,5 @@
 """Example script as in:
-https://github.com/jazzband/docopt-ng/blob/master/examples/more_magic_like.py
+https://github.com/jazzband/docopt-ng/blob/master/examples/more_magic_example.py
 
 $ python examples/more_magic_like.py
 Usage: fmraumyaere [options] [ANAM]...
@@ -13,8 +13,6 @@ Options:
 NOTE: Not ready
 """
 
-from rich import print
-
 from cli_help_maker import generator as gen
 from cli_help_maker.utils import highlight_message
 
@@ -25,11 +23,12 @@ generator = gen.HelpGenerator(
     description_before=False,
     argument_style="all_caps",
     argument_documented_prob=1,
-    argument_repeated=True,  # TODO: Make it a probability
-    arguments_in_section=True,
+    argument_repeated=0.1,  # TODO: Make it a probability
+    arguments_header=True,
+    arguments_section=True,
     options_section=True,
     options_header=True,
-    options_shortcut=True,  # TODO: This should be defined as a probability between programs
+    options_shortcut=0.6,  # TODO: This should be defined as a probability between programs
     option_documented_prob=1.0,
     options_style={"long": True, "style": "all_caps"},
     number_of_options=[1, 2],
