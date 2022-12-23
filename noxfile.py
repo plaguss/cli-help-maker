05d7d133b2b2a8e87e37069c38ed57b8fb96fb1a
agustin <agustin.piqueres@gmail.com>
2022-12-23 19:13:48 +0100

    chore: added commands to run tests and install dependencies

--- noxfile.py
+++ noxfile.py
@@ -1,17 +1,24 @@
 import nox
 
-@nox.session
-def install(session):
-    session.run("flit", "install", "--deps", "production")
+@nox.session(reuse_venv=True)
+def unit_tests(session):
+    session.run("flit", "install", "--deps", "develop")
+    session.run("pytest", "tests/unit")
 
-@nox.session
-def tests(session):
-    # session.install('pytest')
+@nox.session(reuse_venv=True)
+def integration_tests(session):
+    session.run("flit", "install", "--deps", "develop")
     session.run("pytest", "tests/integration")
 
 
+@nox.session(reuse_venv=True)
+def tests_with_coverage(session):
+    session.run("flit", "install", "--deps", "develop")
+    session.run("python", "-m", "pytest", "--cov=cli_help_maker")
+
+
 @nox.session
-def lint(session):
-    # session.install('flake8')
-    session.run("isort", "cli_help_maker", "examples")
-    session.run("black", "cli_help_maker", "examples")
+def format(session):
+    session.run("flit", "install", "--deps", "develop")
+    session.run("isort", "cli_help_maker", "examples", "tests")
+    session.run("black", "cli_help_maker", "examples", "tests")
